a2f5b84b1a43f41c19cabea004ea9beb1a8c52da
gvAbishek <122088021+gvAbishek@users.noreply.github.com>
2024-06-17 23:01:58 +0530

    Add files via upload

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
+} from "react-icons/di";
+import {
+  SiC,
+} from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { FaWordpress } from "react-icons/fa";
+import { TbBrandGolang } from "react-icons/tb";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+      <SiC />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <FaGithub />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <FaWordpress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandGolang />
+      </Col>
+     
+    </Row>
+  );
+}
+
+export default Techstack;
